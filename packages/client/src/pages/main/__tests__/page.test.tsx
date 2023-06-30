50fadf0aa0defcea616e3f11e548b3df0b2f3bd3
YuryBagdasaryan <yabagdas@mts.ru>
2023-06-30 18:09:11 +0300

    [feature/pap-13] добавил тест для основого экрана

---
+++ packages/client/src/pages/main/__tests__/page.test.tsx
@@ -0,0 +1,25 @@
+import { Provider } from 'react-redux';
+import { themeSlice } from '@@entities/theme';
+import { THEMES } from '@@entities/theme/model/contants';
+import { configureStore } from '@reduxjs/toolkit';
+import { render } from '@testing-library/react';
+import { MainPage } from '../ui/Page';
+
+const shoutBeText = `Текущая тема: ${THEMES.light}`;
+
+describe('render page with store', () =>
+  test('should render main page with display theme value', () => {
+    const mockStore = configureStore<RootState>({
+      reducer: {
+        theme: themeSlice.reducer,
+      },
+    });
+
+    const renderApp = render(
+      <Provider store={mockStore}>
+        <MainPage />
+      </Provider>,
+    );
+
+    renderApp.findByDisplayValue(shoutBeText);
+  }));
