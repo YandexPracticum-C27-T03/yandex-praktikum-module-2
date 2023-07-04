5f1fb1be7a55adfb9a5010228fe1e68e51d38297
40thz <stanislav.thz@gmail.com>
2023-07-04 11:13:08 +0300

    Добавил страницу регистрации/компонент формы

---
+++ packages/client/src/shared/lib/constants/register-form-fields.ts
@@ -0,0 +1,28 @@
+import { IForm } from '../types';
+
+export const registerFormFields: IForm[] = [
+  {
+    name: 'first_name',
+    label: 'Имя',
+  },
+  {
+    name: 'second_name',
+    label: 'Фамилия',
+  },
+  {
+    name: 'login',
+    label: 'Логин',
+  },
+  {
+    name: 'email',
+    label: 'Почта',
+  },
+  {
+    name: 'password',
+    label: 'Пароль',
+  },
+  {
+    name: 'phone',
+    label: 'Телефон',
+  },
+];
---
+++ packages/client/src/shared/lib/types.ts
@@ -0,0 +1,5 @@
+import { UseFieldArrayProps } from 'react-hook-form';
+
+export interface IForm extends UseFieldArrayProps {
+  label: string;
+}
