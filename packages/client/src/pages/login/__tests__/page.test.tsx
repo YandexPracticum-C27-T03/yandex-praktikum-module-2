e80daec9b5c1eb3d43ef2c89cd38f895b8f182f1
40thz <stanislav.thz@gmail.com>
2023-07-05 20:17:28 +0300

    Добавил API авторизации/регистрации, подключил к стору

--- packages/client/src/pages/login/__tests__/page.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import { LoginPage } from '../ui/Page';
-
-// const pageContent = 'Login';
-
-test('renders correctly', async () => {
-  // render(<LoginPage />);
-  // expect(screen.getByText(pageContent)).toBeDefined();
-});
