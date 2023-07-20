d745131dd3761f7fbe1b7617d08fc351713b5c34
40thz <stanislav.thz@gmail.com>
2023-07-20 16:36:17 +0700

    рефакторинг

--- packages/client/src/entities/game/ui/fullscreen/fullscreen.tsx
+++
@@ -1,7 +0,0 @@
-import { useFullscreen } from '@@shared/hooks/useFullscreen';
-import { Button } from '@vkontakte/vkui';
-
-export const FullScreen = () => {
-  const handle = useFullscreen();
-  return <Button onClick={() => handle.enter}>test</Button>;
-};
--- packages/client/src/pages/game/ui/Page.tsx
+++ packages/client/src/pages/game/ui/Page.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { GameView } from '@@entities/game';
 import { useFullscreen } from '@@shared/hooks/useFullscreen';
 import { FullScreenContainer } from '@@shared/ui/Fullscreen';
-import { View, Panel, PanelHeader, Group, Button } from '@vkontakte/vkui';
+import { Button } from '@vkontakte/vkui';
+import { GamePreview } from './game-preview';
 
 export const GamePage = () => {
   const handle = useFullscreen();
@@ -10,43 +11,23 @@ export const GamePage = () => {
   const handleStartGame = () => setGameStart(true);
 
   return (
-    <>
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
       <FullScreenContainer handle={handle}>
         {gameStart ? (
           <>
-            <Button style={{ position: 'absolute', top: '2%', left: '1%', background: '#000' }} onClick={handle.enter}>
+            <Button
+              style={{ position: 'absolute', top: '2%', left: '1%', background: '#000', zIndex: 1 }}
+              onClick={handle.enter}
+            >
               Fullscreen
             </Button>
             <GameView />
           </>
         ) : (
-          <View activePanel="start">
-            <Panel id="start">
-              <PanelHeader>2D Runner</PanelHeader>
-              <>
-                <Group title="Описание игры">
-                  Добро пожаловать в захватывающий мир нашего 2D раннера! В этой игре ваша реакция и ловкость будут
-                  стоять на испытании. Вам предстоит стать героем, который должен перепрыгивать разнообразные
-                  препятствия, преодолевая трудности на своем пути. Вам нужно проявить отличную координацию движений,
-                  чтобы перепрыгивать через преграды, которые будут появляться на вашем пути. Ваша задача - собрать как
-                  можно больше бонусов и монет, чтобы получить максимальный счет. Используйте свои навыки и реакцию,
-                  чтобы избегать столкновений. Не забывайте, что каждое препятствие будет появляться все быстрее и
-                  быстрее, поэтому ваше внимание должно быть постоянно направлено на игровое поле. Постарайтесь достичь
-                  высочайшего счета, ставьте рекорды.
-                </Group>
-                <Group>
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Button style={{ width: '200px' }} onClick={handleStartGame}>
-                      Начать игру
-                    </Button>
-                  </div>
-                </Group>
-              </>
-            </Panel>
-          </View>
+          <GamePreview handleStartGame={handleStartGame} />
         )}
       </FullScreenContainer>
-    </>
+    </div>
   );
 };
 
