dd586579cf290a99dd848b2c84debbf86f981d7f
YuryBagdasaryan <yabagdas@mts.ru>
2023-07-21 21:18:21 +0300

    remove fullscreen

--- packages/client/src/pages/game/ui/Page.tsx
+++ packages/client/src/pages/game/ui/Page.tsx
@@ -1,32 +1,20 @@
 import { useState } from 'react';
 import { GameView } from '@@entities/game';
-import { useFullscreen } from '@@shared/hooks/useFullscreen';
-import { FullScreenContainer } from '@@shared/ui/Fullscreen';
-import { Button } from '@vkontakte/vkui';
 import { GamePreview } from './game-preview';
 
 export const GamePage = () => {
-  const handle = useFullscreen();
   const [gameStart, setGameStart] = useState(false);
   const handleStartGame = () => setGameStart(true);
 
   return (
     <div style={{ height: '100vh', overflow: 'hidden' }}>
-      <FullScreenContainer handle={handle}>
-        {gameStart ? (
-          <>
-            <Button
-              style={{ position: 'absolute', top: '2%', left: '1%', background: '#000', zIndex: 1 }}
-              onClick={handle.enter}
-            >
-              Fullscreen
-            </Button>
-            <GameView />
-          </>
-        ) : (
-          <GamePreview handleStartGame={handleStartGame} />
-        )}
-      </FullScreenContainer>
+      {gameStart ? (
+        <>
+          <GameView />
+        </>
+      ) : (
+        <GamePreview handleStartGame={handleStartGame} />
+      )}
     </div>
   );
 };
