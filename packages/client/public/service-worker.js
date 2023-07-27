d01cdb2895bf99662cc1df9310d1030e31d42b68
Dveyn <ananiev_dima.96@mail.ru>
2023-07-27 21:24:24 +0300

    fix

--- packages/client/public/service-worker.js
+++ packages/client/public/service-worker.js
@@ -3,39 +3,59 @@ const assetManifest = self.__WB_MANIFEST || [];
 const STATIC_CACHE_NAME = 'static-data';
 const DYNAMIC_CACHE_NAME = 'dynamic-data';
 
-const manifestURLs = assetManifest.map(
-  (entry) => {
-    return entry.url
-  }
-)
+const manifestURLs = assetManifest.map(entry => entry.url);
 
 const staticAssets = [
   '/',
   ...manifestURLs
 ];
 
-
-self.addEventListener('install', async event => {
-  const cache = await caches.open(STATIC_CACHE_NAME);
-  cache.addAll(staticAssets);
+self.addEventListener('install', event => {
+  event.waitUntil(cacheStaticAssets());
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', event => {
+  event.waitUntil(removeOldCaches());
   return self.clients.claim();
 });
-
 
 self.addEventListener('fetch', event => {
   const { request } = event;
   event.respondWith(cacheData(request));
 });
 
+async function cacheStaticAssets() {
+  const cache = await caches.open(STATIC_CACHE_NAME);
+  return cache.addAll(staticAssets);
+}
+
+async function removeOldCaches() {
+  const cacheKeys = await caches.keys();
+  return Promise.all(
+    cacheKeys
+      .filter(key => key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME)
+      .map(key => caches.delete(key))
+  );
+}
+
 async function cacheData(request) {
-  const cashedRequest = await caches.match(request);
-  if (staticAssets.some(sa => request.url.indexOf(sa) >= 0) || request.headers.get('accept').includes('text/html')) {
-    return cashedRequest || networkFirst(request);
+  const cachedRequest = await caches.match(request);
+  if (cachedRequest) {
+    return cachedRequest;
   }
-  return cashedRequest || networkFirst(request);
+
+  if (shouldCache(request)) {
+    return networkFirst(request);
+  }
+
+  return fetch(request);
+}
+
+function shouldCache(request) {
+  return (
+    staticAssets.some(sa => request.url.indexOf(sa) >= 0) ||
+    request.headers.get('accept').includes('text/html')
+  );
 }
 
 async function networkFirst(request) {
@@ -48,4 +68,3 @@ async function networkFirst(request) {
     return await cache.match(request);
   }
 }
-
